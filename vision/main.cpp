fca6604275d4f950fea6062be0342f21333e54c2
Mulac <wallbridge.calum@gmail.com>
2020-02-07 20:50:26 +0000

    port checkers function

--- vision/main.cpp
+++ vision/main.cpp
@@ -3,15 +3,48 @@
 
 using namespace cv;
 
+class ChkrBoard {
+    int board[8][8]{};
+public:
+    int playRed(int row, int col){
+        if board
+    }
+};
+
+void get_checkers_board(Mat const& img){
+    Mat corners;
+    if( findChessboardCorners(img, Size2i(7,7), corners) )
+        corners = corners.reshape(0, 7);
+    else
+        return;
+
+    Mat extend = Mat::zeros(9,9, corners.type());
+    copyMakeBorder(corners, extend, 1, 1, 1, 1, BORDER_ISOLATED);
+
+    for (int row = 1; row < 8; ++row){
+        extend.row(row).col(0) = 2 * extend.row(row).col(1) - extend.row(row).col(2);
+        extend.row(row).col(8) = 2 * extend.row(row).col(7) - extend.row(row).col(6);
+    }
+
+    extend.row(0) = 2 * extend.row(1) - extend.row(2);
+    extend.row(8) = 2 * extend.row(7) - extend.row(6);
+}
+
 int main() {
-    Mat img;
-    img = imread("../imgs/cntrs.jpg");
-    resize(img, img, Size(), 0.2, 0.2, INTER_AREA);
 
-    namedWindow("Display Image", WINDOW_AUTOSIZE);
-    imshow("Display Image", img);
+    ChkrBoard board;
 
-    waitKey(0);
+    std::cout << board.board[1][1] << std::endl;
+//    Mat img;
+//    img = imread("../imgs/cntrs.jpg");
+//    resize(img, img, Size(), 0.2, 0.2, INTER_AREA);
+//
+//    get_checkers_board(img);
+//
+//    namedWindow("Display Image", WINDOW_AUTOSIZE);
+//    imshow("Display Image", img);
+//
+//    waitKey(0);
 
     return 0;
 }
