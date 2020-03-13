aa5744faafebbe681b957899fad74d97c0034ba4
Mulac <wallbridge.calum@gmail.com>
2020-03-13 14:42:38 +0000

    Moved loop from play to main

--- main.py
+++ main.py
@@ -11,45 +11,47 @@ def setup():
     global corners
     ret, img = cap.read()
 
-    if isinstance(game, NineMensMorris):
+    # Find the corners to transform the camera frames
+    if corners is None and isinstance(game, NineMensMorris):
         corners = game.build_board(vision.find_board, img)
     else:
-        game.build_board(vision.find_board, img)
+        raise Exception("Please show me NMM board first")
+
+    # Find the new relative intersections of the board
+    game.build_board(vision.find_board, vision.deskew(img, corners))
 
 
 def play(representation):
-    while True:
-        # representation.show()
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        frame = vision.deskew(frame, corners)
+    # representation.show()
+    ret, frame = cap.read()
+    frame = vision.deskew(frame, corners)
 
-        winner = representation.is_end()
-        if winner:
-            print(winner, "WINS")
-            break
+    counters, cimg = vision.find_counters(frame)
+    if counters is not None:  # Counters have started being placed
+        board = representation.compute_state(counters)
+        if representation.is_valid_move_state(board):
+            representation.update_board(board)
+            representation.show()
 
-        counters, cimg = vision.find_counters(frame)
-        if counters is not None:  # Counters have started being placed
-            board = representation.compute_state(counters)
-            if representation.is_valid_move_state(board):
-                representation.update_board(board)
-                representation.show()
+    # Display the resulting frame
+    vision.cv2.imshow('counters', cimg)
 
-        # Display the resulting frame
-        vision.cv2.imshow('counters', cimg)
-        if vision.cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    winner = representation.is_end()
+    if winner:
+        print("\nThe Winner is: {}".format(winner))
+        return False
+    return True
 
 
 if __name__ == "__main__":
-    game = NineMensMorris()
-
-    cap = vision.cv2.VideoCapture(0)
+    game = NineMensMorris()             # Create the board game
+    cap = vision.cv2.VideoCapture(0)    # Open the camera
 
     setup()
-    play(game)
 
-    # When everything done, release the capture
-    cap.release()
-    vision.cv2.destroyAllWindows()
+    while play(game):
+        if vision.cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()                       # Close the camera
+    vision.cv2.destroyAllWindows()      # Close all windows
